39fda4c2950d1de53f1c343f4f6029fc0773a97d
j0suesv <99438333+j0suesv@users.noreply.github.com>
2022-11-15 20:30:39 -0500

    Add files via upload

---
+++ form-code.js
@@ -0,0 +1,7 @@
+function obtenerValores(){
+    nombre= String(document.getElementById("name").value);
+    email = String(document.getElementById("email").value);
+    subject = String(document.getElementById("subject").value);
+    message = String(document.getElementById("message").value);
+    alert("Mensaje enviado")
+}
